1b28bbbec02ca95b1d79890c6066344da6dcfac9
Asena4 <asenakevin@gmail.com>
2024-04-29 13:43:46 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# A puppet manifest installing flask v2.1.0 which is a package from pip3.
-# Also specifying the version of flask to install using the ensure attribute
+# A puppet manifest installing flask v2.1.0 which is a package from pip3
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
