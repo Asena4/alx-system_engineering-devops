1f73f951c16d5e35cd7a88d0c607b6c48f51d4c7
Kevin Asena <asenakevin@gmail.com>
2024-06-07 04:37:44 -0700

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-
 """
-Queries the Reddit API
-and returns the number of subscribers for a given subreddit
+Script that queries subscribers on a given Reddit subreddit.
 """
 
-import base64
 import requests
-from urllib.parse import quote
-
-BASE = 'https://www.reddit.com/r/'
-CLIENT_ID = "gjV8WEVc3awcbN00hZLVpdD6gbo3_A"
 
 
 def number_of_subscribers(subreddit):
-    """function"""
-    headers = {
-        'User-Agent': 'my-app/0.0.1/220942',
-        'Authorization':
-        "Basic " +
-        base64.b64encode((quote(CLIENT_ID) + ":").encode()).decode()}
-    data = requests.get(BASE + str.format("{}/about.json",
-                        subreddit), headers=headers,
-                        allow_redirects=False)
-    if data.status_code != 200:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    return data.json().get('data').get('subscribers')
